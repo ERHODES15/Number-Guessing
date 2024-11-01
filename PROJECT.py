797af65716ff3410fd935d8f8f2cc7d11f354722
ERHODES15 <edrhode1@boe.jack.k12.wv.us>
2024-11-01 14:48:32 +0000

    number guesser

--- PROJECT.py
+++ PROJECT.py
@@ -6,18 +6,22 @@ name = input("What is your name? ")
 print(f"Hey {name} it is nice to meet you.")
 print("Lets play a game.")
 print(" Pick a number 1 to 10 and i will tell you if it is higher or lower.")
-while play == "Yes" or "yes":
+while play == "Yes" or "yes":    
+    tries = 0
     answer = random.randrange(1,10)
     while guess != answer:
+        tries += 1
         guess = input("What is your guess ")
         guess = int(guess) 
+        if guess > 10:
+            print("Guess is to high try again")
+        elif guess < 1:
+            print("Guess is to low try again")
         if guess > answer:
             print("the answer is lower")
-            tries =+ 1
             continue 
         elif guess < answer:
             print("the answer is higher")
-            tries =+ 1
             continue
         elif guess == answer:
             print(f"You got it right good job, It took you {tries} tries")
